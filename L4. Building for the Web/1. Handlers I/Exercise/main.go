b8402aa721651b1968793a90069b1b6af9aad260
fraden <dennisfrankenbach@gmail.com>
2023-06-26 10:19:26 +0200

    feat: solved L4 Exercises

--- L4. Building for the Web/1. Handlers I/Exercise/main.go
+++ L4. Building for the Web/1. Handlers I/Exercise/main.go
@@ -1,7 +1,26 @@
 package main
 
-// var cities = []string{"Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City"}
+import (
+	"fmt"
+	"net/http"
+)
+
+var cities = []string{"Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City"}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
+func cityList(w http.ResponseWriter, r *http.Request) {
+	for _, city := range cities {
+		fmt.Fprintf(w, "%s\n", city)
+	}
+}
 
 func main() {
-	// TODO
+	http.HandleFunc("/", index)
+	http.HandleFunc("/citylist", cityList)
+
+	http.ListenAndServe(":3000", nil)
+
 }
--- L4. Building for the Web/3. Handlers III/Exercise/main.go
+++ L4. Building for the Web/3. Handlers III/Exercise/main.go
@@ -1,13 +1,28 @@
 package main
 
-// var cityPopulations = map[string]uint32{
-// 	"Tokyo":       37435191,
-// 	"Delhi":       29399141,
-// 	"Shanghai":    26317104,
-// 	"Sao Paulo":   21846507,
-// 	"Mexico City": 21671908,
-// }
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var cityPopulations = map[string]uint32{
+	"Tokyo":       37435191,
+	"Delhi":       29399141,
+	"Shanghai":    26317104,
+	"Sao Paulo":   21846507,
+	"Mexico City": 21671908,
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(cityPopulations)
+}
 
 func main() {
-	// TODO
+	http.HandleFunc("/", index)
+	fmt.Println("Server starts on port 3000...")
+	http.ListenAndServe(":3000", nil)
 }
